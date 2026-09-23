192f6b6a5ba39f8978b3c5e61d47f3f983e214fb
thiyageshvenkat <thiyageshvenkat@users.noreply.github.com>
2026-09-23 15:41:00 -0700

    chore(pedro): migrate PsiKit demo pose type

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/lib/PsiKitDemo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/lib/PsiKitDemo.java
@@ -1,6 +1,6 @@
 package org.firstinspires.ftc.teamcode.opmodes.tests.lib;
 
-import com.pedropathing.geometry.Pose;
+import com.pedropathing.math.Pose;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Utility;
 
