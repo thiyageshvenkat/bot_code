c05962d998e5a433867ef12e6a5fc27565378328
thiyageshvenkat <thiyageshvenkat@users.noreply.github.com>
2026-09-23 16:16:43 -0700

    feat(auto): define mirrored preload shoot and park route

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BiobuzzAutoPlan.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import com.pedropathing.math.Pose;
+import com.pedropathing.paths.Path;
+
+import org.firstinspires.ftc.teamcode.constants.RobotConfig;
+import org.firstinspires.ftc.teamcode.game.AllianceColor;
+
+import static com.pedropathing.api.Paths.line;
+
+/** Tunable preload-to-hive-to-loading-zone route mirrored by alliance. */
+public final class BiobuzzAutoPlan {
+    public final Pose start;
+    public final Pose shoot;
+    public final Pose park;
+    public final Path toShoot;
+    public final Path toPark;
+
+    private BiobuzzAutoPlan(Pose start, Pose shoot, Pose park) {
+        this.start = start;
+        this.shoot = shoot;
+        this.park = park;
+        toShoot = line(start, shoot).linear(start, shoot);
+        toPark = line(shoot, park).linear(shoot, park);
+    }
+
+    public static BiobuzzAutoPlan forAlliance(AllianceColor alliance) {
+        Pose blueStart = degrees(RobotConfig.Auto.START_X,
+                RobotConfig.Auto.START_Y, RobotConfig.Auto.START_HEADING_DEG);
+        Pose blueShoot = degrees(RobotConfig.Auto.SHOOT_X,
+                RobotConfig.Auto.SHOOT_Y, RobotConfig.Auto.SHOOT_HEADING_DEG);
+        Pose bluePark = degrees(RobotConfig.Auto.PARK_X,
+                RobotConfig.Auto.PARK_Y, RobotConfig.Auto.PARK_HEADING_DEG);
+        if (alliance == AllianceColor.BLUE) {
+            return new BiobuzzAutoPlan(blueStart, blueShoot, bluePark);
+        }
+        return new BiobuzzAutoPlan(mirrorAcrossCenterline(blueStart),
+                mirrorAcrossCenterline(blueShoot), mirrorAcrossCenterline(bluePark));
+    }
+
+    private static Pose degrees(double x, double y, double headingDegrees) {
+        return new Pose(x, y, Math.toRadians(headingDegrees));
+    }
+
+    private static Pose mirrorAcrossCenterline(Pose blue) {
+        return new Pose(-blue.x(), blue.y(), Math.PI - blue.heading());
+    }
+}
